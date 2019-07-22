a90b48a5f1ee98396e13d696fae39eefaf9c17e4
rizwan456 <rizzsheikh786@gmail.com>
2019-07-22 11:14:54 +0530

    Initial commit

---
+++ app/src/main/java/droids/rizz/dagger2example/MainActivity.java
@@ -0,0 +1,42 @@
+package droids.rizz.dagger2example;
+
+import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import javax.inject.Inject;
+
+import droids.rizz.dagger2example.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
+    private MyComponent myComponent;
+
+    @Inject
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        myComponent=DaggerMyComponent.builder().sharedPrefModule(new SharedPrefModule(this)).build();
+        myComponent.inject(this);
+        setUp();
+    }
+
+    private void setUp() {
+        mainBinding.btnGet.setOnClickListener(v->{
+            mainBinding.inUsername.setText(sharedPreferences.getString("UserName","DEFAULT"));
+            mainBinding.inNumber.setText(sharedPreferences.getString("Number","123456"));
+        });
+
+        mainBinding.btnSave.setOnClickListener(v->{
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            editor.putString("UserName",mainBinding.inUsername.getText().toString());
+            editor.putString("Number",mainBinding.inNumber.getText().toString());
+            editor.apply();
+        });
+    }
+}
---
+++ app/src/main/java/droids/rizz/dagger2example/SharedPrefModule.java
@@ -0,0 +1,42 @@
+package droids.rizz.dagger2example;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+
+/*
+    @Module : This is used on the class that does the work of constructing objects that’ll be eventually provided as dependencies.
+    @Provides : This is used on the methods inside the Module class that’ll return the object.
+    @Inject : This is used upon a constructor, field or a method and indicates that dependency has been requested.
+    @Component : The Module class doesn’t provide the dependency directly to the class that’s requesting it. For this, a Component interface is used that acts as a bridge between @Module and @Inject.
+    @Singleton : This indicates that only a single instance of the dependency object would be created.
+*/
+
+
+@Module
+public class SharedPrefModule {
+    private Context context;
+
+    public SharedPrefModule(Context context) {
+        this.context = context;
+    }
+
+    @Singleton
+    @Provides
+    public Context provideContex() {
+        return context;
+    }
+
+    @Singleton
+    @Provides
+    public SharedPreferences provideSharePrefrances(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+}
